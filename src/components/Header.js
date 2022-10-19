5167fbe85b3f828bb4f59a7ce3b2ef4283bfe696
TheReaLLsQuaLL <150316039@ogr.cbu.edu.tr>
2022-10-20 02:52:43 +0300

    feat - now when you click on the name of the website links to main page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import ProductListing from "./ProductListing";
 
 const Header = () => {
   return (
     <div className="ui fixed menu">
       <div className="ui container center">
-        <h2>MarketPlace</h2>
+        <Link to={`/`}>MarketPlace</Link>
       </div>
     </div>
   );
